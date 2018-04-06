777ebba26c01588728c32ac6d71e7b4774383d1f
wydra76yan <yan@vydra.com>
2018-04-06 14:08:49 +0300

    Fixed Todd Add Todo list

--- src/component/content.jsx
+++ src/component/content.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import '../component-css/content.css';
 
 
-const Todo =({todo,remove,description}) =>{
+const Todo =({todo, remove, description}) =>{
   return(
     <li class="todo">
       <p>{todo.text}</p>
+      <p>{todo.description}</p>
       <div>
         <button>Des</button>
         <button>Del</button>
@@ -14,12 +15,20 @@ const Todo =({todo,remove,description}) =>{
   );
 }
 
+const TodoList = ({todos, remove, description}) => {
+  const todoNode = todos.map((todo) => {
+    return (<Todo todo={todo} key={todo.id} remove={remove} description={description}/>)
+  });
+  return (<ul>{todoNode}</ul>);
+}
 
 export default class Content extends React.Component{
   render(){
     return (
       <div class="content">
-        <Todo/>
+        <TodoList todos={
+            [{id:1, text:'kek', description:'First TODO'},{id:2, text:'lol', description:'Second TODO'}]
+          }/>
       </div>
     );
   }
