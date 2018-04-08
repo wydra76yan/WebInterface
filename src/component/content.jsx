b09b3e676952eec3dbb7751612ce18698775f674
wydra76yan <yan@vydra.com>
2018-04-08 15:43:57 +0300

    Adding todos to list

--- src/component/content.jsx
+++ src/component/content.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
 import '../component-css/content.css';
+import TodoList from './todoList.jsx';
 
 
-const Todo =({todo, remove, description}) =>{
-  return(
-    <li class="todo">
-      <p>{todo.text}</p>
-      <p>{todo.description}</p>
-      <div>
-        <button>Des</button>
-        <button>Del</button>
-      </div>
-    </li>
-  );
-}
+// const Todo =({todo, remove, description}) =>{
+//   return(
+//     <li class="todo">
+//       <p>{todo.text}</p>
+//       <p>{todo.description}</p>
+//       <div>
+//         <button>Des</button>
+//         <button>Del</button>
+//       </div>
+//     </li>
+//   );
+// }
+//
+// addTodo(e){
+//   e.preventDefault();
+// }
+//
+// const TodoList = ({todos, remove, description}) => {
+//   const todoNode = todos.map((todo) => {
+//     return (<Todo todo={todo} key={todo.id} remove={remove} description={description}/>)
+//   });
+//   return (<ul class="todoList">{todoNode}</ul>);
+// }
 
-const TodoList = ({todos, remove, description}) => {
-  const todoNode = todos.map((todo) => {
-    return (<Todo todo={todo} key={todo.id} remove={remove} description={description}/>)
-  });
-  return (<ul class="todoList">{todoNode}</ul>);
-}
+
 
 export default class Content extends React.Component{
   render(){
     return (
       <div class="content">
-        <TodoList todos={
-            [{id:1, text:'kek', description:'First TODO'},{id:2, text:'lol', description:'Second TODO'},{id:3, text:'lol', description:'Third TODO'}]
-          }/>
+        <TodoList/>
       </div>
-    );
+    )
   }
 }
